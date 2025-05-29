eeff3b6124cc73f4d6db7ce7135c834cc71a4634
Naseer Khan <naseer@edatasolutions.com.au>
2025-05-29 15:25:36 +0500

    userAuthService fixed

--- services/userAuthService/src/services/jwtService.js
+++ services/userAuthService/src/services/jwtService.js
@@ -1,4 +1,4 @@
-const {generateToken,verifyToken} = require('../utils/tokenUtils')
+const { generateToken, verifyToken } = require('../utils/tokenUtils');
 const User = require('../models/User');
 const {
   ACCESS_TOKEN_SECRET,
@@ -7,25 +7,32 @@ const {
   REFRESH_TOKEN_EXPIRES_IN,
 } = process.env;
 
-// Access token functions
+// Access token
 const createAccessToken = (user) =>
-  generateToken({ id: user._id, email: user.email }, ACCESS_TOKEN_SECRET, ACCESS_TOKEN_EXPIRES_IN);
+  generateToken(
+    { id: user._id, email: user.email, type: 'access' },
+    ACCESS_TOKEN_SECRET,
+    ACCESS_TOKEN_EXPIRES_IN
+  );
+
+// Refresh token
+const createRefreshToken = (user) =>
+  generateToken(
+    { id: user._id, type: 'refresh' },
+    REFRESH_TOKEN_SECRET,
+    REFRESH_TOKEN_EXPIRES_IN
+  );
 
 const verifyAccessToken = (token) =>
   verifyToken(token, ACCESS_TOKEN_SECRET, 'Invalid or expired access token');
 
-// Refresh token functions
-const createRefreshToken = (user) =>
-  generateToken({ id: user._id }, REFRESH_TOKEN_SECRET, REFRESH_TOKEN_EXPIRES_IN);
-
 const verifyRefreshToken = (token) =>
   verifyToken(token, REFRESH_TOKEN_SECRET, 'Invalid or expired refresh token');
 
-// Service to refresh the access token using refresh token
 const generateAccessToken = async (refreshToken) => {
   try {
     const decoded = verifyRefreshToken(refreshToken);
-    if (!decoded) {
+    if (!decoded || decoded.type !== 'refresh') {
       throw new Error('Invalid refresh token');
     }
 
@@ -35,6 +42,11 @@ const generateAccessToken = async (refreshToken) => {
     }
 
     const accessToken = createAccessToken(user);
+    // Optional rotation:
+    // const newRefreshToken = createRefreshToken(user);
+    // user.refreshToken = newRefreshToken;
+    // await user.save();
+
     return { accessToken };
   } catch (err) {
     throw new Error('Invalid refresh token');
@@ -46,5 +58,5 @@ module.exports = {
   createRefreshToken,
   verifyAccessToken,
   verifyRefreshToken,
-  generateAccessToken
+  generateAccessToken,
 };
